ccc0bbd94e0a903b28b3bc4258015ceb64ae39a4
kacase <emil@9di.de>
2021-01-26 19:28:13 +0100

    added comments for pipe

--- src/app/custom.pipe.ts
+++ src/app/custom.pipe.ts
@@ -2,6 +2,9 @@ import { Pipe, PipeTransform } from "@angular/core";
 @Pipe({ name: "customPipe" })
 export class CustomPipe implements PipeTransform {
   transform(value: number, parameter?: any): any {
+    // 5.2 Implement logic here to return the correct string
+    // use value as the number of bytes
+    // use parameter as the number of decimals to display
     return "";
   }
 }
